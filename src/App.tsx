b6b9c4630039994e51248c418275e79afccb3840
astropsy999 <bareust@gmail.com>
2023-04-05 19:28:29 +0300

    Store, Set auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Component } from 'solid-js';
 import AppRoutes from './router';
+import { useAuthState } from './components/context/auth';
 
 const App: Component = () => {
+  const authState = useAuthState()!
   return (
     <>
+    <div class="text-white">
+      <div>Is Authenticated: {`${authState.isAuthenticated}`}</div>
+      <div>Is Loading: {`${authState.loading}`}</div>
+    </div>
     <div id="popups"></div>
       <AppRoutes />
     </>
--- src/components/context/auth.tsx
+++ src/components/context/auth.tsx
@@ -1,20 +1,42 @@
 import { Accessor, Component, ParentComponent, createContext, createSignal, onCleanup, onMount, useContext } from "solid-js";
+import {createStore} from 'solid-js/store'
 
 type AuthStateCtxValues = {
-    isAuthenticated: Accessor<boolean>
-    loading: Accessor<boolean>
+    isAuthenticated: boolean
+    loading: boolean
 
 }
 
+const initialState = () => ({
+ isAuthenticated: false,
+ loading: true
+}
+)
 const AuthStateContext = createContext<AuthStateCtxValues>();
 
 const AuthProvider: ParentComponent = (props) => {
 
-    const [isAuthenticated, setIsAuthenticated] = createSignal(false)
-    const [loading, setLoading] = createSignal(true)
+    const [store, setStore] = createStore(initialState())
 
-onMount(() => {
+onMount(async () => {
+  try {
+    await authenticateUser();
+  } catch (error: any) {
+    console.log(error);
+  } finally {
+    setStore('loading', false)
+  }
 })
+
+const authenticateUser = async() => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+        setStore("isAuthenticated", true)
+        // res(true)
+        rej("problem")
+    }, 1000)
+  })
+}
 
 onCleanup(() => {
 
@@ -22,10 +44,7 @@ onCleanup(() => {
 
 return (
   <AuthStateContext.Provider
-    value={{
-      isAuthenticated,
-      loading,
-    }}
+    value={store}
   >
     {props.children}
   </AuthStateContext.Provider>
